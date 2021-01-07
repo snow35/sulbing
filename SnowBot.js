9d01a7bf3a8c6ef0aa5b6f1a5bcc3d36cd8c1186
snow35 <hijack0305@naver.com>
2021-01-08 04:17:32 +0900

    도움말 이모지 추가

--- SnowBot.js
+++ SnowBot.js
@@ -37,11 +37,11 @@ const WarrStrawSulbingInfo = new Discord.MessageEmbed()
 
 const BotcommandHelp = new Discord.MessageEmbed()
 	.setColor('#ddbea9')
-	.setTitle(':shaved_ice:인절미빙수 봇 명령어입니다')
+	.setTitle('인절미빙수 봇 명령어입니다')
 	.addFields(
-		{ name: '!빙수정보 (빙수이름)', value: '원하는 빙수의 정보를 알려줘요'},
-		{ name: '!빙수메뉴판', value: '현재 등록된 빙수를 보여줘요\n!빙수, !메뉴판 으로도 호출가능해요'},
-		{ name: '!빙수주문 (빙수이름)', value: '원하는 빙수를 주문할수 있어요!\n**아직 제작중이랍니당**'},
+		{ name: ':shaved_ice:!빙수정보 (빙수이름)', value: '원하는 빙수의 정보를 알려줘요'},
+		{ name: ':clipboard:!빙수메뉴판', value: '현재 등록된 빙수를 보여줘요\n!빙수, !메뉴판 으로도 호출가능해요'},
+		{ name: ':man_rasing_hand:!빙수주문 (빙수이름)', value: '원하는 빙수를 주문할수 있어요!\n**아직 제작중이랍니당**'},
 	);
 
 const Icelist = new Discord.MessageEmbed()
