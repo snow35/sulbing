e6a52792e208e04c31de850d0ad9acb0fe0517b4
snow35 <hijack0305@naver.com>
2021-01-16 04:44:51 +0900

    Update SnowBot.js

--- SnowBot.js
+++ SnowBot.js
@@ -1,15 +1,10 @@
 const Discord = require('discord.js');
 const fs = require("fs");
 const client = new Discord.Client();
-const privateMessage = require('./private-message')
-
-var onedaysold = 0; // 하루판매량 변수선언 -> node-schedule 사용해서 하루에 한 번씩 변수 초기화 by 페넥님 (암튼 이거두 수정하면 됨)
-
 function getBingsuMenu() {
 	let data = fs.readFileSync("./menu.json");
 	return JSON.parse(data).bingSu;
 }
-
 //명령어 설명
 const BotcommandHelp = new Discord.MessageEmbed()
 	.setColor('#ddbea9')
@@ -19,24 +14,24 @@ const BotcommandHelp = new Discord.MessageEmbed()
 		{ name: ':clipboard:!빙수메뉴판', value: '현재 등록된 빙수를 보여줘요\n!메뉴판 으로도 호출가능해요'},
 		{ name: ':man_raising_hand:!주문 (빙수이름)', value: '원하는 빙수를 주문할수 있어요!'}
 	);
-
 //테스트 코드
 client.on('ready', () => {
     console.log('ready!');
 });
-
 //입력값 확인, 실행
 client.on('message', message => {
 	console.log(message.content);
-	//이거 실제 ping으로 수정하긔
+	//테스트 호출
 	if (message.content === '!ping') {
 		message.channel.send('Pong');
 	}
 
 	else if (message.content.startsWith("!빙수 ")) {
+		let args = message.content.split(" ");
 		let command = message.content.split(" ")[0];
 		let menu = getBingsuMenu();
 		menu.forEach(item => {
+			if (args[1] == item.name) {
 			if (message.content.replace(command, "").trim() == item.name) {
 				let bingSuInfoEmbed = new Discord.MessageEmbed()
 					.setColor('#ddbea9')
@@ -52,15 +47,13 @@ client.on('message', message => {
 						"나트륨(mg): " + item.nutrition.sodium)
 					.addField("알레르기", item.allergy.join(", "));
 				message.channel.send(bingSuInfoEmbed);
-			}
+			}}
 		});
 	}
-
 	//도움말 호출
 	else if ( message.content === '!점장' || message.content === '!점장님' || message.content === '!점장나와!') {
 		message.channel.send(BotcommandHelp);
 	}
-
 	//메뉴판호출
 	else if ( message.content === '!빙수메뉴판' || message.content === '!메뉴판') {
 		let bingsuListEmbed = new Discord.MessageEmbed()
@@ -72,29 +65,17 @@ client.on('message', message => {
 		});
 		message.channel.send(bingsuListEmbed);
 	}
-
+	//멘션 테스트
+	else if (message.content === '!호출') {
+		message.channel.send("<@"+message.author+">님")
+	}
 	else if (message.content.slice(0,3) === '!주문') {
 		message.channel.send("<@"+message.author+">님")
 		message.channel.send(message.content.slice(4,)+" 주문되었습니다")
 		message.channel.send("소요시간은 약3~5분 입니다")
-		onedaysold++;
 	}
-
-	//호출 테스트 지움
-
-	/* 여기 하루 판매량 기능 추가중임
-	else if (message.content.slice(0,6) === '!오늘판매량') {
-		message.channel.send("<@"+message.author+">님")
-		message.channel.send("오늘 판매량은"+onedaysold+"개 입니다")
-		message.channel.send("소요시간은 약3~5분 입니다")
-		onedaysold++;
-	}
-	*/
-
 	else if (message.content === '!루님화이팅') {
 		message.channel.send("루님화이티이잉!")
 	}
-	privateMessage(client, 'Send_DM', 'hello!')
 });
-
 client.login(process.env.TOKEN);
