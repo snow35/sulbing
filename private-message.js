74a5eabdf968c4e7ca868ffe02f550fa97cfbe90
snow35 <hijack0305@naver.com>
2021-01-16 05:43:20 +0900

    Update private-message.js

--- private-message.js
+++ private-message.js
@@ -1,9 +1,7 @@
 const { Client } = require("discord.js");
 
-module.exports = (Client, triggertext, replytext) => {
+module.exports = (Client, replytext) => {
     Client.on('message', message => {
-        if(message.content.toLowerCase() === triggertext.toLowerCase()){
             message.author.send(replytext)
-        }
     })
 }
