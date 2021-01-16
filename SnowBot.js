bcf53e9485c0efcc5f9072e1451ef758b66c8a5b
snow35 <hijack0305@naver.com>
2021-01-16 19:27:55 +0900

    DM기능 테스트

--- SnowBot.js
+++ SnowBot.js
@@ -23,6 +23,11 @@ client.on('ready', () => {
 //입력값 확인, 실행
 client.on('message', message => {
 	console.log(message.content);
+
+	client.users.fetch('516590788382162945').then((user) => {
+		user.send('hello world!')
+	})
+
 	//테스트 호출
 	if (message.content === '!ping') {
 		message.channel.send('Pong');
