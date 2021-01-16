9dc93674a77b1fc8710c16d59844b65fe25c6328
snow35 <hijack0305@naver.com>
2021-01-16 21:30:33 +0900

    DM기능 조건문으로 다시 수정
    추가 파일 안 불러오고 봇 js파일 내부에서 조건문으로 DM기능 수행하는걸로 수정함
    메세지 보낸 사람 봇인지 자기자신인지 비교하는것도 넣음

--- SnowBot.js
+++ SnowBot.js
@@ -5,7 +5,7 @@ function getBingsuMenu() {
 	let data = fs.readFileSync("./menu.json");
 	return JSON.parse(data).bingSu;
 }
-const privateMessage = require('./private-message')
+//const privateMessage = require('./private-message')
 
 //명령어 설명
 const BotcommandHelp = new Discord.MessageEmbed()
@@ -21,9 +21,14 @@ client.on('ready', () => {
     console.log('ready!');
 });
 
-//문제해결코드?
+//보낸사람 = 봇일떄
 client.on("message", message => {
     if (message.author.bot) return;
+});
+
+//보낸사람 = 자기자신 일때
+client.on("message", message => {
+    if (client.user.id == message.author.id) return;
 });
 
 //입력값 확인, 실행
@@ -84,6 +89,9 @@ client.on('message', message => {
 	}
 	else if (message.content === '!루님화이팅') {
 		message.channel.send("루님화이티이잉!")
+	}
+	else if (message.content === '!DM') {
+		message.author.send("hello!")
 	}
 	
 });
