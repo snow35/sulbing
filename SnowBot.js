923289be4e59eb9c991ab453a3b6f28036e35a18
snow35 <hijack0305@naver.com>
2021-01-18 05:13:06 +0900

    테스트3

--- SnowBot.js
+++ SnowBot.js
@@ -184,10 +184,16 @@ client.on('message', message => {
 			user.send("Player2's card is"+card_shape.Player_two_card_shape+card_dict.Player_two_card)
 		});
 
+		const Spade = bot.emojis.cache.get("800032748625592340")
+		message.channel.send(`${Spade}`)
 		client.users.fetch(secondPlayerUserId).then((user)=>{
 			user.send("Hi, "+secondPlayerUsername)
-			user.send("Player1's card is"+'${800032748625592340}'+[card_dict.Player_one_card])
+			user.send("Player1's card is"+'${emoji}'+card_dict[Player_one_card])
 		});
+	}
+	else if(message.content === '!test'){
+		const Spade = bot.emojis.cache.get("800032748625592340")
+		message.channel.send(`${Spade}`)
 	}
 
 });
