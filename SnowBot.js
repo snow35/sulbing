e25678f28e32d695f28fc594a7faa1b07ce3b0dd
snow35 <hijack0305@naver.com>
2021-01-18 05:26:29 +0900

    테스트6

--- SnowBot.js
+++ SnowBot.js
@@ -193,7 +193,7 @@ client.on('message', message => {
 	}
 	else if(message.content === '!test'){
 		const Spade = client.emojis.cache.get("800032748625592340")
-		message.channel.send('${Spade}')
+		message.channel.send(Spade)
 		message.channel.send(card_dict[5])
 	}
 
