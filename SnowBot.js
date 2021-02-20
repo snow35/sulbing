69bc455a03e59348c9c7897291029cc9508c7ce3
snow35 <69392206+snow35@users.noreply.github.com>
2021-02-20 12:35:09 +0900

    플레이리스트 넣고 에러 떳는데 어딘지 몰라서 일단 수정해봄

--- SnowBot.js
+++ SnowBot.js
@@ -225,6 +225,6 @@ client.on('message', (message) => {
       .addFields(
         { name: 'https://www.youtube.com/playlist?list=PL81QvWaskBuv3NdYolkknz7EskmsGoDe0D', value: '설\'s_pop 플레이리스트 입니다(신나는.ver)' },
         { name: 'https://www.youtube.com/playlist?list=PL81QvWaskBuujAYez-K3TIyLeEIcuEn2M', value: '설\'s_pop_2탄 플레이리스트 입니다(잔잔한.ver)' }
-                       ));
+                       );
 });
 client.login(process.env.TOKEN);
