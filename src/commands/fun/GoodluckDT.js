b8957d57d3e0bef79959f2467e3050fa3a465397
Hee Sul Shin <69392206+snow35@users.noreply.github.com>
2021-04-28 17:21:32 +0900

    독님 응원 메세지 수정
    루님꺼 복붙만하고 커밋해버려서 수정했습니다

--- src/commands/fun/GoodluckDT.js
+++ src/commands/fun/GoodluckDT.js
@@ -3,14 +3,14 @@ const { Command } = require('discord.js-commando');
 module.exports = class GoodLuckLuCommand extends Command {
   constructor(client) {
     super(client, {
-      name: '루님화이팅',
+      name: '독님화이팅',
       group: 'fun',
-      memberName: '루님화이팅',
-      description: '루님화이티이잉!',
+      memberName: '독님화이팅',
+      description: '독님화이티이잉!',
     });
   }
 
   async run(msg) {
-    msg.say('루님화이티이잉!');
+    msg.say('독님화이티이잉!');
   }
 };
