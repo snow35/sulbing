e61caec389c966b5faffa40936e0ab7b3f7c6f57
FennecFoxSW <FennecFoxSW@protonmail.com>
2021-04-30 14:34:35 +0900

    Change command prefix
    ! -> s!

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const log = new CatLoggr().setLevel('debug');
 
 const client = new commando.Client({
   owner: ['516590788382162945', '427328263896039435', '439817891240607746'],
-  commandPrefix: '!',
+  commandPrefix: 's!',
 });
 
 const messageDelete = require('./logEvents/messageDelete');
