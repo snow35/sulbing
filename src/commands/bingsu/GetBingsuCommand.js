82dbeee288827897ca2f0dd2ad458d8c27a4fcf9
FennecFoxSW <FennecFoxSW@protonmail.com>
2021-05-03 10:53:10 +0900

    Edit embedBingsuInfo function

--- src/commands/bingsu/GetBingsuCommand.js
+++ src/commands/bingsu/GetBingsuCommand.js
@@ -11,25 +11,23 @@ function getBingsuMenu() {
 
 function embedBingsuInfo(bingsu) {
   const menu = getBingsuMenu();
-  menu.forEach((item) => { // TODO: 이거 return 수정
-    if (item.subname.includes(bingsu)) {
-      const bingSuInfoEmbed = new Discord.MessageEmbed()
-        .setColor('#ddbea9')
-        .setTitle(item.name)
-        .setThumbnail(item.img)
-        .setDescription(item.description)
-        .addField('제품명', item.name, true)
-        .addField('가격', `${item.cost}원`, true)
-        .addField('영양성분',
-          stripIndent`열량(Kcal): ${item.nutrition.calories}
-          당류(g): ${item.nutrition.sugars}
-          단백질(g): ${item.nutrition.protein}
-          포화지방(g): ${item.nutrition.saturatedFat}
-          나트륨(mg): ${item.nutrition.sodium}`)
-        .addField('알레르기', item.allergy.join(', '));
-      return bingSuInfoEmbed;
-    }
-  });
+  const item = menu.filter(itemOfMenu => itemOfMenu.subname.includes(bingsu))[0];
+  console.log(item);
+  const bingSuInfoEmbed = new Discord.MessageEmbed()
+    .setColor('#ddbea9')
+    .setTitle(item.name)
+    .setThumbnail(item.img)
+    .setDescription(item.description)
+    .addField('제품명', item.name, true)
+    .addField('가격', `${item.cost}원`, true)
+    .addField('영양성분',
+      stripIndent`열량(Kcal): ${item.nutrition.calories}
+      당류(g): ${item.nutrition.sugars}
+      단백질(g): ${item.nutrition.protein}
+      포화지방(g): ${item.nutrition.saturatedFat}
+      나트륨(mg): ${item.nutrition.sodium}`)
+    .addField('알레르기', item.allergy.join(', '));
+  return bingSuInfoEmbed;
 }
 
 module.exports = class GetBingsuCommand extends Command {
